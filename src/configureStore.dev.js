c9629cefa7f5888ab70b35971eb3dd42672589f8
Dan Erikson <derikson3@gmail.com>
2016-08-01 22:48:21 -0600

    Update logger to better display immutable data

--- src/configureStore.dev.js
+++ src/configureStore.dev.js
@@ -3,7 +3,16 @@
 import { applyMiddleware, createStore } from 'redux';
 import list from './redux/modules/list';
 import createLogger from 'redux-logger';
+import { Iterable } from 'immutable';
 
 export default function configureStore(init?: any) {
-  return createStore(list, init, applyMiddleware(createLogger()));
+  const stateTransformer = (state) => {
+    if (Iterable.isIterable(state)) {
+      return state.toJS();
+    } else {
+      return state;
+    }
+  };
+  const logger = createLogger({ stateTransformer });
+  return createStore(list, init, applyMiddleware(logger));
 }
